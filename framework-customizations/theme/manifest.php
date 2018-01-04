e99d058d884b1f5c43c8088677d84fca785eaeec
Yurii Pavlov <mailyurik@gmail.com>
2018-01-04 19:36:06 +0200

    update

---
+++ framework-customizations/theme/manifest.php
@@ -0,0 +1,13 @@
+<?php
+
+	if ( ! defined( 'FW' )) {
+		exit;
+	}
+
+	$manifest = array();
+	$manifest['id'] = 'bvc';
+
+	$manifest['supported_extensions'] = array(
+		'sidebars'		=> array(),
+		'breadcrumbs'	=> array(),
+	);
