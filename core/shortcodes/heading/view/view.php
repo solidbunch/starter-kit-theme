4d5ab26011fe7eb57abd9c41a32ee3abf0d0c6f1
Viktor Sedzialo <viktor.sedzialo@gmail.com>
2018-07-20 15:23:03 +0300

    Fixed settings property in shortcodes

--- core/shortcodes/heading/view/view.php
+++ core/shortcodes/heading/view/view.php
@@ -13,8 +13,8 @@ extract( shortcode_atts( array(
 	'css' => ''
 ), $atts ) );
 
-$css_class = apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, vc_shortcode_custom_css_class( $css, ' ' ), $this->settings['base'], $atts );
-$css_class .= ' ' . $atts['classes'];
+$css_class = $this->get_css_class( $css, $data );
+
 ?>
 <<?php echo esc_attr( $atts['heading'] ); ?> id="<?php echo esc_attr( $data['id'] ); ?>" class="<?php echo esc_attr( $css_class ); ?>">
 <?php echo wp_kses_post( $atts['title'] ); ?>
