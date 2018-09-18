237849827ceba0a31ed9bba905ac06f25a402484
Nikita Bolotov <zyv4yk@gmail.com>
2018-09-18 15:26:09 +0300

    setup webpack for compile scss from widgets/shortcodes/assets folders

--- assets/build/core.config.js
+++ assets/build/core.config.js
@@ -2,15 +2,18 @@ const baseConfig = require('./_basic.config');
 // include the css extraction and minification plugins
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-return {
-    entry: {
-        front : './assets/css/front/front.scss',
-        admin :'./assets/css/admin/admin.scss',
+module.exports = Object.assign(
+    {
+        name: 'core',
+        entry: {
+            front: './assets/css/front/front.scss',
+            admin: './assets/css/admin/admin.scss',
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: './assets/css/[name]/[name].css'
+            }),
+        ],
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './assets/css/[name]/[name].css'
-        }),
-    ],
     baseConfig
-};
+);
