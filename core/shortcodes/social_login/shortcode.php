7cb5ec0c376d8c3f2ae73985f2ddc47d0f23f6e1
Dimon <d.kudleichuk@gmail.com>
2018-09-20 11:55:45 +0300

    OAuth with Twitter, Facebook, Google + social login buttons shortcode

---
+++ core/shortcodes/social_login/shortcode.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Social Login Shortcode
+ **/
+
+if ( class_exists( 'WPBakeryShortCode' ) ) {
+	class WPBakeryShortCode_Social_Login extends WPBakeryShortCode {
+		
+		protected function content( $atts, $content = null ) {
+			
+			$shortcode_dir = dirname( __FILE__ );
+			$shortcode     = basename( $shortcode_dir );
+			$shortcode_uri = \ffblank\helper\utils::get_shortcodes_uri( $shortcode );
+			
+			$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+			
+			if ( ! empty( $atts['el_id'] ) ) {
+				$id = 'shortcode-' . $atts['el_id'];
+			} else {
+				$id = '';
+			}
+			
+			wp_enqueue_style( 'font-awesome' );
+			wp_enqueue_style( 'shortcode-social-login', \ffblank\helper\utils::get_shortcodes_uri( $shortcode, '/assets/style.css') );
+			
+			$data = array(
+				'id'      => $id,
+				'atts'    => $atts,
+				'content' => $content,
+				'wpb'     => $this
+			);
+			
+			return FFBLANK()->view->load( '/view/view', $data, true, $shortcode_dir );
+			
+		}
+		
+	}
+}
--- framework-customizations/theme/options/social.php
+++ framework-customizations/theme/options/social.php
@@ -7,6 +7,50 @@ $options = array(
 			'type' => 'tab',
 			'options' => array(
 
+				'oauth-settings-box' => array(
+					'title'   => esc_html__( 'Social Login', 'fruitfulblanktextdomain' ),
+					'type'    => 'box',
+					'attr'    => array(
+						'class' => 'prevent-auto-close'
+					),
+					'options' => array(
+
+						'facebook_app_id' => array(
+							'type'  => 'text',
+							'label' => esc_html__( 'Facebook Application ID', 'fruitfulblanktextdomain' ),
+							'value' => ''
+						),
+						'facebook_app_secret' => array(
+							'type'  => 'text',
+							'label' => esc_html__( 'Facebook Application Secret', 'fruitfulblanktextdomain' ),
+							'value' => ''
+						),
+
+						'google_client_id' => array(
+							'type'  => 'text',
+							'label' => esc_html__( 'Google Client ID', 'fruitfulblanktextdomain' ),
+							'value' => ''
+						),
+						'google_client_secret' => array(
+							'type'  => 'text',
+							'label' => esc_html__( 'Google Client Secret', 'fruitfulblanktextdomain' ),
+							'value' => ''
+						),
+
+						'twitter_consumer_key' => array(
+							'type'  => 'text',
+							'label' => esc_html__( 'Twitter Consumer Key', 'fruitfulblanktextdomain' ),
+							'value' => ''
+						),
+						'twitter_consumer_secret' => array(
+							'type'  => 'text',
+							'label' => esc_html__( 'Twitter Consumer Secret', 'fruitfulblanktextdomain' ),
+							'value' => ''
+						),
+
+					)
+				),
+
 				'social_profiles-settings-box' => array(
 					'title'   => esc_html__( 'Social Profiles', 'fruitfulblanktextdomain' ),
 					'type'    => 'box',
