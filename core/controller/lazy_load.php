7cf46c3c57b27da69613ee502db2333506c5fba6
Vitalik <vitalik@matesmarketing.com>
2018-10-03 18:04:26 +0300

    update lazy load

--- core/controller/lazy_load.php
+++ core/controller/lazy_load.php
@@ -52,7 +52,8 @@ class lazy_load {
 			add_filter( 'get_avatar', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
 			add_filter( 'widget_text', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
 			add_filter( 'get_image_tag', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
-			add_filter( 'wp_get_attachment_image_attributes', array( $this, 'process_image_attributes' ), PHP_INT_MAX );
+			//add_filter( 'wp_get_attachment_image_attributes', array( $this, 'process_image_attributes' ), PHP_INT_MAX );
+			add_filter( 'ff_media_img_html', array( $this, 'process_image_attributes'), PHP_INT_MAX);
 
 			// Load scripts
 			add_action( 'wp_enqueue_scripts', array( $this, 'load_assets' ) );
@@ -70,12 +71,6 @@ class lazy_load {
 	 * Check if we need to run lazy load actions
 	 */
 	public function skip() {
-
-		// If this is the admin page, do nothing.
-		if ( is_admin() ) {
-			return true;
-		}
-
 
 		$is_img_lazy_load_enabled = (int) \ffblank\helper\utils::get_option( 'img_lazy_load', 1 );
 
@@ -110,9 +105,7 @@ class lazy_load {
 		}
 
 		// Find all <img> elements via regex, add lazy-load attributes.
-		$content = preg_replace_callback( '#<(img)([^>]+?)(>(.*?)</\\1>|[\/]?>)#si', array( $this, 'process_image' ),
-			$content );
-
+		$content = preg_replace_callback( '#<(img)([^>]+?)(>(.*?)</\\1>|[\/]?>)#si', array( $this, 'process_image' ), $content );
 		return $content;
 
 	}
@@ -131,6 +124,9 @@ class lazy_load {
 		if ( empty( $old_attributes_kses_hair['src'] ) ) {
 			return $matches[0];
 		}
+		if ( !empty( $old_attributes_kses_hair['data-src'] ) ) {
+			return $matches[0];
+		}
 
 		$old_attributes = $this->flatten_kses_hair_data( $old_attributes_kses_hair );
 		$new_attributes = $this->process_image_attributes( $old_attributes );
@@ -144,21 +140,6 @@ class lazy_load {
 		return sprintf( '<img %1$s><noscript>%2$s</noscript>', $new_attributes_str, $matches[0] );
 	}
 
-	/**
-	 * Flatten attribute list into string.
-	 *
-	 * @param array $attributes Array of attributes.
-	 *
-	 * @return array $flattened_attributes
-	 */
-	public function flatten_kses_hair_data( $attributes ) {
-		$flattened_attributes = array();
-		foreach ( $attributes as $name => $attribute ) {
-			$flattened_attributes[ $name ] = $attribute['value'];
-		}
-
-		return $flattened_attributes;
-	}
 
 	/**
 	 * Given an array of image attributes, updates the `src`, `srcset`, and `sizes` attributes so
@@ -168,43 +149,109 @@ class lazy_load {
 	 *
 	 * @return array The updated image attributes array with lazy load attributes.
 	 */
-	public function process_image_attributes( $attributes ) {
-
+	public function process_image_attributes( $attributes) {
+		
 		if ( empty( $attributes['src'] ) ) {
 			return $attributes;
 		}
+		$old_attributes = $attributes;
+		
 		if ( ! empty( $attributes['class'] ) && $this->should_skip_image_with_blacklisted_class( $attributes['class'] ) ) {
 			return $attributes;
 		}
-		if ( ! empty( $attributes['data-src'] ) ) {
+		
+		$image_size['width'] = $min_width = 24;
+		$image_size['height'] = $min_height = 24;
+		
+		if ( isset( $attributes['width']) && isset( $attributes['height'] ) && !empty( $attributes['width'] && $attributes['height'] ) ) {
+			$image_size['width'] = $attributes['width'];
+			$image_size['height'] = $attributes['height'];
+			
+		} elseif ( isset($attributes['data-width'] ) && isset( $attributes['data-height'] ) && !empty( $attributes['data-width'] && $attributes['data-height'] )) {
+			$image_size['width'] = $attributes['data-width'];
+			$image_size['height'] = $attributes['data-height'];
+			
+		} elseif ( preg_match('/([\d]+)x([\d]+)\.(jpg|png|jpeg|gif)/i', $attributes['src'], $size_match)) {
+			$image_size['width'] = $size_match[1];
+			$image_size['height'] = $size_match[2];
+			
+		} elseif ( \ffblank\helper\utils::get_option('lazy_load_get_sizes_with_getimagesize', 1) ) {
+			$image_size = getimagesize($old_attributes['src']);
+			
+			if(!empty($image_size[0] && $image_size[1])){
+				$image_size['width'] = $image_size[0];
+				$image_size['height'] = $image_size[1];
+			}
+			
+		}
+		
+		if ($image_size['width'] < $min_width && $image_size['height'] < $min_height) {
 			return $attributes;
 		}
-
-		$old_attributes = $attributes;
-
+		
+		$attributes['src'] = $this->get_placeholder_image( $image_size['width'], $image_size['height']);
+		
 		// Add the lazy class to the img element.
 		$attributes['class'] = $this->set_lazy_class( $attributes );
-
-		// Set placeholder and lazy-src.
-		$attributes['src'] = $this->get_placeholder_image();
-
+		
 		// Set data-src to the original source uri.
 		$attributes['data-src'] = $old_attributes['src'];
-
+		
 		// Process `srcset` attribute.
-		if ( ! empty( $attributes['srcset'] ) && ! empty( $attributes['data-srcset'] ) ) {
+		if ( ! empty( $attributes['srcset'] ) ) {
 			$attributes['data-srcset'] = $old_attributes['srcset'];
 			unset( $attributes['srcset'] );
 		}
+		
 		// Process `sizes` attribute.
 		if ( ! empty( $attributes['sizes'] ) ) {
 			$attributes['data-sizes'] = $old_attributes['sizes'];
 			unset( $attributes['sizes'] );
 		}
-
+		
 		return $attributes;
 	}
+	
 
+	/**
+	 * Append a `lazy` class to <img> elements for lazy-loading.
+	 *
+	 * @param array $attributes <img> element attributes.
+	 *
+	 * @return string
+	 */
+	public function set_lazy_class( $attributes ) {
+		if ( !empty( $attributes['class'] ) ) {
+			$classes  = $attributes['class'];
+			$classes .= ' lazy-loading';
+		} else {
+			$classes = 'lazy-loading';
+		}
+		
+		return $classes;
+	}
+
+	/**
+	 * Set the placeholder image.
+	 *
+	 * @return string The URL to the placeholder image.
+	 */
+	function get_placeholder_image($image_width = 24, $image_height = 24) {
+		
+		$placeholder_color = \ffblank\helper\utils::get_option('placeholder_color', '#555');
+		
+		$data = array(
+			'width' => (int)$image_width,
+			'height' => (int)$image_height,
+			'fill' => $placeholder_color,
+		);
+		
+		$svg = base64_encode(FFBLANK()->view->load('/template-parts/lazy-loading-svg', $data, true));
+		
+		$placeholder = "data:image/svg+xml;base64," . $svg;
+		return $placeholder;
+	}
+	
 	/**
 	 * Returns true when a given string of classes contains a class signifying image
 	 * should not be lazy-loaded
@@ -217,41 +264,30 @@ class lazy_load {
 		$blacklisted_classes = array(
 			'skip-lazy',
 		);
-
+		
 		foreach ( $blacklisted_classes as $class ) {
 			if ( false !== strpos( $classes, $class ) ) {
 				return true;
 			}
 		}
-
+		
 		return false;
 	}
-
+	
 	/**
-	 * Append a `lazy` class to <img> elements for lazy-loading.
+	 * Flatten attribute list into string.
 	 *
-	 * @param array $attributes <img> element attributes.
+	 * @param array $attributes Array of attributes.
 	 *
-	 * @return string
+	 * @return array $flattened_attributes
 	 */
-	public function set_lazy_class( $attributes ) {
-		if ( array_key_exists( 'class', $attributes ) ) {
-			$classes = $attributes['class'];
-			$classes .= ' lazy';
-		} else {
-			$classes = 'lazy';
+	public function flatten_kses_hair_data( $attributes ) {
+		$flattened_attributes = array();
+		foreach ( $attributes as $name => $attribute ) {
+			$flattened_attributes[ $name ] = $attribute['value'];
 		}
-
-		return $classes;
-	}
-
-	/**
-	 * Set the placeholder image.
-	 *
-	 * @return string The URL to the placeholder image.
-	 */
-	public function get_placeholder_image() {
-		return get_theme_file_uri( 'assets/images/lazy/placeholder.svg' );
+		
+		return $flattened_attributes;
 	}
 
 	/**
@@ -278,21 +314,15 @@ class lazy_load {
 	 * Load lazy JS
 	 */
 	public function load_assets() {
-		wp_enqueue_script( 'ffblank-lazy-load-images', FFBLANK()->config['assets_uri'] . 'js/lazyload.js', array(),
-			FFBLANK()->config['cache_time'], false );
-		wp_script_add_data( 'ffblank-lazy-load-images', 'defer', true );
+		wp_enqueue_script( 'lazy-load', FFBLANK()->config['assets_uri'] . 'js/lazyload.js', array(),FFBLANK()->config['cache_time'], false );
+		wp_script_add_data( 'lazy-load', 'defer', true );
 	}
 
 	/**
 	 * Do not lazy load avatar in admin bar.
 	 */
 	public function remove_filters() {
-		remove_filter( 'the_content', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
-		remove_filter( 'post_thumbnail_html', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
 		remove_filter( 'get_avatar', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
-		remove_filter( 'widget_text', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
-		remove_filter( 'get_image_tag', array( $this, 'add_image_placeholders' ), PHP_INT_MAX );
-		remove_filter( 'wp_get_attachment_image_attributes', array( $this, 'process_image_attributes' ), PHP_INT_MAX );
 	}
 
 	/**
--- framework-customizations/theme/options/general.php
+++ framework-customizations/theme/options/general.php
@@ -8,16 +8,16 @@ $options = array(
 			'options' => array(
 
 				'img_lazy_load_box' => array(
-					'title'   => esc_html__( 'Extra', 'fruitfulblanktextdomain' ),
+					'title'   => esc_html__( 'Lazy Load Options', 'fruitfulblanktextdomain' ),
 					'type'    => 'box',
 					'attr'    => array(
 						'class' => 'prevent-auto-close'
 					),
 					'options' => array(
-
+					
 						'img_lazy_load' => array(
 							'type'         => 'switch',
-							'label'        => __( 'Lazy Load Images', 'fruitfulblanktextdomain' ),
+							'label'        => __( ' Lazy Load Enable/Disable', 'fruitfulblanktextdomain' ),
 							'right-choice' => array(
 								'value' => '1',
 								'label' => __( 'Yes', 'fruitfulblanktextdomain' )
@@ -27,8 +27,28 @@ $options = array(
 								'color' => '#ccc',
 								'label' => __( 'No', 'fruitfulblanktextdomain' )
 							),
-							'desc'         => __( 'Lazy Load Images on site', 'fruitfulblanktextdomain' ),
+							'desc'         => __( 'Lazy Load Images on/off site', 'fruitfulblanktextdomain' ),
 
+						),
+						'placeholder_color' => array(
+							'type' => 'color-picker',
+							'label' => __('Placeholder color', 'fruitfulblanktextdomain'),
+							'value' => '#555',
+							'desc'         => __( 'Add placeholder color' ),
+						),
+						'lazy_load_get_sizes_with_getimagesize' => array(
+							'type' => 'switch',
+							'right-choice' => array(
+								'value' => '1',
+								'label' => __('Yes', 'fruitfulblanktextdomain'),
+							),
+							'left-choice'  => array(
+								'value' => '0',
+								'color' => '#ccc',
+								'label' => __( 'No', 'fruitfulblanktextdomain' )
+							),
+							'desc'         => __( 'Try to get image sizes with getimagesize() if there is no width and heght attributes.
+						 Attantion! php function getimagesize() can significantly slow down your site speed. Use neatly', 'fruitfulblanktextdomain' ),
 						),
 
 					)
