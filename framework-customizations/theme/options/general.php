bd735303310b92eb906d8a4c279686f8fcdf35a4
Nikita Lysenko <mazanicker@gmail.com>
2018-10-03 12:05:08 +0300

    Restored files

--- framework-customizations/theme/options/general.php
+++ framework-customizations/theme/options/general.php
@@ -34,6 +34,33 @@ $options = array(
 					)
 				),
 
+				'antispam_box' => array(
+					'title'   => esc_html__( 'Antispam', 'fruitfulblanktextdomain' ),
+					'type'    => 'box',
+					'attr'    => array(
+						'class' => 'prevent-auto-close'
+					),
+					'options' => array(
+
+						'forms_antispam'	=> array(
+							'type'  => 'switch',
+							'label' => __('Antispam', 'fruitfulblanktextdomain'),
+							'right-choice' => array(
+								'value' => '1',
+								'label' => __('Yes', 'fruitfulblanktextdomain')
+							),
+							'left-choice' => array(
+								'value' => '0',
+								'color' => '#ccc',
+								'label' => __('No', 'fruitfulblanktextdomain')
+							),
+							'desc'  => __('Antispam for all Email Forms', 'fruitfulblanktextdomain'),
+
+						),
+
+					)
+				),
+
 			)
 		)
 	)
