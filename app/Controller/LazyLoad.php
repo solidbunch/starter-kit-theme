9e36eb62bafeb7e3ab322032c02229b109c74df8
VSedzialo <viktor.sedzialo@gmail.com>
2018-12-21 14:41:49 +0200

    Edited http://solidbunch.com -> https://solidbunch.com

--- app/Controller/LazyLoad.php
+++ app/Controller/LazyLoad.php
@@ -9,7 +9,7 @@ namespace StarterKit\Controller;
  * @category   Wordpress
  * @package    Starter Kit Frontend
  * @author     SolidBunch
- * @link       http://solidbunch.com
+ * @link       https://solidbunch.com
  * @version    Release: 1.0.0
  * @since      Class available since Release 1.0.0
  */
--- app/Controller/Shortcodes.php
+++ app/Controller/Shortcodes.php
@@ -10,7 +10,7 @@ use StarterKit\Helper\Utils;
  * @category   Wordpress
  * @package    Starter Kit Backend
  * @author     SolidBunch
- * @link       http://solidbunch.com
+ * @link       https://solidbunch.com
  * @version    Release: 1.0.0
  * @since      Class available since Release 1.0.0
  */
