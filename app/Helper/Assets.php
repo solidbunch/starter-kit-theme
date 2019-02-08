c9de891a3ddb74770742061c7ad162ca17e68b24
Nikita Bolotov <zyv4yk@gmail.com>
2019-02-08 18:26:46 +0200

    started to refactoring Assets helper, added dist folder for precompiled assets available

--- app/Helper/Assets.php
+++ app/Helper/Assets.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace StarterKit\Helper;
 
 /**
@@ -16,8 +15,9 @@ namespace StarterKit\Helper;
  * @since      Class available since Release 1.0.0
  */
 class Assets {
+
 	/**
-	 * Enqueue shortcode style
+	 * Enqueue style from dist folder
 	 *
 	 * @param $handle
 	 * @param string $src
@@ -25,19 +25,60 @@ class Assets {
 	 * @param mixed $ver
 	 * @param string $media
 	 */
-	public static function enqueue_style( $handle, $src = '', $deps = array(), $ver = false, $media = 'all' ) {
-		if ( $ver === false ) {
-			/** @var int $ver - timestamp */
-			$ver = filemtime( get_stylesheet_directory() . '/dist/css/' . $src );
-		}
-
-		$src = get_stylesheet_directory_uri() . '/dist/css/' . $src;
-
-		wp_enqueue_style( $handle, $src, $deps, $ver, $media );
+	public static function enqueue_style_dist(
+		$handle,
+		$src = '',
+		$deps = array(),
+		$ver = false,
+		$media = 'all'
+	) {
+		self::enqueue_style( $handle, '/dist/css' . $src, $deps, $ver, $media );
 	}
 
 	/**
-	 * Enqueue shortcode script
+	 * Enqueue style
+	 *
+	 * @param $handle
+	 * @param string $src
+	 * @param array $deps
+	 * @param mixed $ver
+	 * @param string $media
+	 */
+	public static function enqueue_style(
+		$handle,
+		$src = '',
+		$deps = array(),
+		$ver = false,
+		$media = 'all'
+	) {
+		$src = get_stylesheet_directory_uri() . '/' . $src;
+		$ver = self::add_version( $src, $ver );
+
+		if ( file_exists( $src ) ) {
+			wp_enqueue_style( $handle, $src, $deps, $ver, $media );
+		}
+	}
+
+	/**
+	 * set timestamp to $ver var if false provided
+	 *
+	 * @param $src
+	 * @param $ver
+	 *
+	 * @return int
+	 */
+	protected static function add_version( $src, $ver ) {
+		$remove_versions = utils::get_option( 'assets_versions', false );
+		if ( $ver === false && file_exists( $src ) && ! $remove_versions ) {
+			/** @var int $ver - timestamp */
+			$ver = filemtime( $src );
+		}
+
+		return $ver;
+	}
+
+	/**
+	 * register script for usage in future
 	 *
 	 * @param $handle
 	 * @param string $src
@@ -45,14 +86,83 @@ class Assets {
 	 * @param mixed $ver
 	 * @param bool $in_footer
 	 */
-	public static function enqueue_script( $handle, $src = '', $deps = array(), $ver = false, $in_footer = false ) {
-		if ( $ver === false ) {
-			/** @var int $ver - timestamp */
-			$ver = filemtime( get_stylesheet_directory() . '/dist/js/' . $src );
+	public static function register_script(
+		$handle,
+		$src = '',
+		$deps = array(),
+		$ver = false,
+		$in_footer = false
+	) {
+		$src = get_stylesheet_directory_uri() . '/' . $src;
+		$ver = self::add_version( $src, $ver );
+		if ( file_exists( $src ) ) {
+			wp_register_script( $handle, $src, $deps, $ver, $in_footer );
 		}
+	}
 
-		$src = get_stylesheet_directory_uri() . '/dist/js/' . $src;
+	/**
+	 * register style for usage in future
+	 *
+	 * @param $handle
+	 * @param string $src
+	 * @param array $deps
+	 * @param mixed $ver
+	 * @param string $media
+	 */
+	public static function register_style(
+		$handle,
+		$src = '',
+		$deps = array(),
+		$ver = false,
+		$media = 'all'
+	) {
+		$src = get_stylesheet_directory_uri() . '/' . $src;
+		$ver = self::add_version( $src, $ver );
 
-		wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+		if ( file_exists( $src ) ) {
+			wp_register_style( $handle, $src, $deps, $ver, $media );
+		}
+	}
+
+	/**
+	 * Enqueue script from dist folder
+	 *
+	 * @param $handle
+	 * @param string $src
+	 * @param array $deps
+	 * @param mixed $ver
+	 * @param bool $in_footer
+	 */
+	public static function enqueue_script_dist(
+		$handle,
+		$src = '',
+		$deps = array(),
+		$ver = false,
+		$in_footer = false
+	) {
+		self::enqueue_script( $handle, '/dist/js' . $src, $deps, $ver, $in_footer );
+	}
+
+	/**
+	 * Enqueue script
+	 *
+	 * @param $handle
+	 * @param string $src
+	 * @param array $deps
+	 * @param mixed $ver
+	 * @param bool $in_footer
+	 */
+	public static function enqueue_script(
+		$handle,
+		$src = '',
+		$deps = array(),
+		$ver = false,
+		$in_footer = false
+	) {
+		$src = get_stylesheet_directory_uri() . '/' . $src;
+		$ver = self::add_version( $src, $ver );
+		if ( file_exists( $src ) ) {
+			wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+		}
 	}
 }
