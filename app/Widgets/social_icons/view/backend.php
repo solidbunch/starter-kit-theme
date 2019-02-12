56517b2a427201d61e1af38c481497870f9300a7
vvv3 <test.vvv@ukr.net>
2019-02-12 03:56:29 +0200

    Widgets : Code refactoring

--- app/Widgets/social_icons/view/backend.php
+++ app/Widgets/social_icons/view/backend.php
@@ -1,6 +1,15 @@
+<?php
+/**
+ * @var \WP_Widget[]|array[] $data   
+ */
+?>
+
 <p>
-	<label for="<?php echo esc_attr( $data['widget']->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Widget title:', 'starter-kit' ); ?></label>
-	<input type="text" class="widefat title" id="<?php echo esc_attr( $data['widget']->get_field_id( 'title' ) ); ?>"
-	       name="<?php echo esc_attr( $data['widget']->get_field_name( 'title' ) ); ?>"
-	       value="<?php echo esc_attr( $data['instance']['title'] ); ?>"/>
+	<label for="<?php echo esc_attr( $data['widget']->get_field_id( 'title' ) ); ?>">
+		<?php esc_html_e( 'Widget title:', 'starter-kit' ); ?>
+	</label>
+	<input type="text" class="widefat title"
+		   id="<?php echo esc_attr( $data['widget']->get_field_id( 'title' ) ); ?>"
+		   name="<?php echo esc_attr( $data['widget']->get_field_name( 'title' ) ); ?>"
+		   value="<?php echo esc_attr( $data['instance']['title'] ); ?>"/>
 </p>
