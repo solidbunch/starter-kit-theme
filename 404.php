1082de022cc951e4181699e5739e78b1f7740d00
DK <d.kudleichuk@gmail.com>
2019-03-18 14:34:55 +0200

    actions / filters namespace changed

--- 404.php
+++ 404.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-	<?php do_action( 'starter-kit/layout_start'); ?>
+	<?php do_action( 'StarterKit/layout_start'); ?>
 
 		<?php
-			do_action( 'starter-kit/before_page_404_content');
-			do_action( 'starter-kit/page_404_content');
-			do_action( 'starter-kit/after_page_404_content');
+			do_action( 'StarterKit/before_page_404_content');
+			do_action( 'StarterKit/page_404_content');
+			do_action( 'StarterKit/after_page_404_content');
 		?>
 
-	<?php do_action( 'starter-kit/layout_end'); ?>
+	<?php do_action( 'StarterKit/layout_end'); ?>
 
 <?php get_footer(); ?>
