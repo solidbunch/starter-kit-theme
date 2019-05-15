49bba7634698903c10f772eb290432ad9c3fd74b
yurii-pavlov <mailyurik@gmail.com>
2019-05-15 18:16:25 +0300

    add google analytics script to options

--- framework-customizations/theme/options/analytics.php
+++ framework-customizations/theme/options/analytics.php
@@ -21,6 +21,29 @@ $options = [
 							'attr'  => [ 'placeholder' => 'GTM-XXXXXXX' ],
 							'value' => ''
 						],
+
+						'analytics_code' => [
+							'type'  => 'text',
+							'label' => esc_html__( 'Analytics Code', 'starter-kit' ),
+							'attr'  => [ 'placeholder' => 'UA-XXXXXXXXX-X' ],
+							'value' => '',
+							'desc'  => __( 'For a better speed performance, please insert the analytics code through the tag manager. Turn on google Analytics Scripts Local Load option' , 'starter-kit' ),
+						],
+
+						'analytics_js_lazy_load' => [
+							'type'         => 'switch',
+							'label'        => __( 'Analytics Scripts Local Load', 'starter-kit' ),
+							'right-choice' => [
+								'value' => '1',
+								'label' => __( 'Yes', 'starter-kit' )
+							],
+							'left-choice'  => [
+								'value' => '0',
+								'color' => '#ccc',
+								'label' => __( 'No', 'starter-kit' )
+							],
+							'desc'         => __( 'Load Tag Manager and Analytics scripts from local directory', 'starter-kit' ),
+						],
 					
 					]
 				],
