6aec3b927656784fcf6146af83c564e030ccdd2f
yurii-pavlov <mailyurik@gmail.com>
2019-06-12 18:54:46 +0300

    header nav update styles - become wide

--- template-parts/header/header-default.php
+++ template-parts/header/header-default.php
@@ -1,69 +1,16 @@
 <header id="top">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
 
-	<div class="container">
+					<div class="navbar-brand">
 
-		<nav class="navbar">
+						<h1 class="text-primary">
+							<a href="<?php echo site_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
+						</h1>
 
-			<div class="navbar-collapse w-100 order-1 order-md-0 dual-collapse2">
+						<small><?php bloginfo( 'description' ); ?></small>
 
-				<div class="navbar-brand">
+					</div>
 
-					<h1 class="text-primary">
-						<a href="<?php echo site_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
-					</h1>
-
-					<small><?php bloginfo( 'description' ); ?></small>
-
-				</div>
-
-			</div>
-
-		</nav>
-
-	</div>
-
-</header>
-
-<section class="bg-light">
-
-	<?php if ( has_nav_menu( 'header_menu' ) ) : ?>
-
-		<div class="container">
-
-			<nav id="main-nav" class="navigation-menu">
-
-				<div class="d-md-block d-lg-none order-3">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'header_menu',
-							'menu_class'     => 'mobile-menu',
-							'walker'          => new wp_bootstrap_navwalker()
-						)
-					);
-					?>
-				</div>
-
-				<div class="w-100 order-3">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'header_menu',
-							'menu_class'     => 'desktop-menu',
-							'walker'          => new wp_bootstrap_navwalker()
-						)
-					);
-					?>
-				</div>
-
-				<button class="menu-button" type="button">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-			</nav>
-
-			<nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
-					<!-- Brand and toggle get grouped for better mobile display -->
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-header-navbar-collapse" aria-controls="bs-header-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
@@ -74,15 +21,15 @@
 						'container'         => 'div',
 						'container_class'   => 'collapse navbar-collapse',
 						'container_id'      => 'bs-header-navbar-collapse',
-						'menu_class'        => 'nav navbar-nav',
+						'menu_class'        => 'nav navbar-nav ml-auto',
 						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
 						'walker'            => new WP_Bootstrap_Navwalker(),
 					) );
 					?>
+
+
 			</nav>
 
-		</div>
+	</nav>
 
-	<?php endif; ?>
-
-</section>
+</header>
