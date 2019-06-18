e27d46dc85eac88c190a3376041418b8b5263edf
Yurii Pavlov <mailyurik@gmail.com>
2019-06-18 21:56:24 +0300

    multilevel dropdown main menu + mobile

--- template-parts/header/header-default.php
+++ template-parts/header/header-default.php
@@ -1,33 +1,30 @@
-<header id="top" class="bg-light">
+<header class="main-header bg-light">
 	<div class="container">
-	<nav class="navbar navbar-expand-lg navbar-light bg-light pl-0 pr-0" role="navigation">
+		<nav class="header-menu navbar navbar-expand-lg navbar-light bg-light pl-0 pr-0" role="navigation">
 
-					<div class="navbar-brand">
+			<div class="navbar-brand">
 
-						<a class="text-primary" href="<?php echo site_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
-						<small><?php bloginfo( 'description' ); ?></small>
+				<a class="text-primary" href="<?php echo site_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
+				<small><?php bloginfo( 'description' ); ?></small>
 
-					</div>
+			</div>
 
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-header-navbar-collapse" aria-controls="bs-header-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<?php
-					wp_nav_menu( array(
-						'theme_location'    => 'header_menu',
-						'depth'             => 3,
-						'container'         => 'div',
-						'container_class'   => 'collapse navbar-collapse',
-						'container_id'      => 'bs-header-navbar-collapse',
-						'menu_class'        => 'nav navbar-nav ml-auto',
-						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-						'walker'            => new WP_Bootstrap_Navwalker(),
-					) );
-					?>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-header-navbar-collapse" aria-controls="bs-header-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location'    => 'header_menu',
+				'depth'             => 4,
+				'container'         => 'div',
+				'container_class'   => 'collapse navbar-collapse',
+				'container_id'      => 'bs-header-navbar-collapse',
+				'menu_class'        => 'nav navbar-nav ml-auto',
+				'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+				'walker'            => new WP_Bootstrap_Navwalker(),
+			) );
+			?>
 
-			</nav>
-
-	</nav>
+		</nav>
 	</div>
-
 </header>
