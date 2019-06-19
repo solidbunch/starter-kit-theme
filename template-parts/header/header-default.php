f1f2bd9f1f34fd99da052a1662b322761dcbd7f4
yurii-pavlov <mailyurik@gmail.com>
2019-06-19 17:12:37 +0300

    navbar-toggler tweaks

--- template-parts/header/header-default.php
+++ template-parts/header/header-default.php
@@ -9,7 +9,7 @@
 
 			</div>
 
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-header-navbar-collapse" aria-controls="bs-header-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#bs-header-navbar-collapse" aria-controls="bs-header-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<?php
