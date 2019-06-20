5cf7482ea75b1cca28391d3072b05048addcbeba
yurii-pavlov <mailyurik@gmail.com>
2019-06-20 23:20:30 +0300

    mobile menu click tweaks

--- assets/js/front-components/header-menu.js
+++ assets/js/front-components/header-menu.js
@@ -20,23 +20,14 @@ export default class HeaderMenu {
      **/
     setupHeaderMenu() {
 
-        if ( !$('.main-header .navbar-toggler').is(":visible") ) {
-            $('.dropdown-toggle').attr('data-toggle', '');
+        if ( $('.main-header .navbar-toggler').is(":visible") ) {
 
-        } else {
-
-            $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+            $('.dropdown-menu .dropdown-toggle-split').on('click', function (e) {
                 if (!$(this).next().hasClass('show')) {
                     $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
                 }
                 var $subMenu = $(this).next(".dropdown-menu");
                 $subMenu.toggleClass('show');
-
-
-                $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
-                    $('.dropdown-submenu .show').removeClass("show");
-                });
-
 
                 return false;
             });
