b8b0e19344d06c50b610ac23829f06335a8f1184
Yurii Pavlov <mailyurik@gmail.com>
2019-06-26 17:54:25 +0300

    footer menu fixes and mobile fixes

--- template-parts/footer/footer-default.php
+++ template-parts/footer/footer-default.php
@@ -33,18 +33,20 @@
 					<div class="col-md-12">
 						<div class="row">
 							<div class="col-md-6 my-auto copyright"><?php echo \StarterKit\Helper\Utils::get_option( 'bottom_bar_text' ); ?></div>
-							<nav class="col-md-6 links">
-
+							<nav class="col-md-6 navbar navbar-dark bg-dark" role="navigation">
 								<?php
-
-									wp_nav_menu(array(
+									wp_nav_menu( [
 										'menu' => 'bottom_bar_menu',
-										'depth' => 1
-									));
-
+										'depth' => 1,
+										'container' => false,
+										//startbootstrapmenu
+										'menu_class'        => 'nav ml-auto',
+										'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+										'walker'            => new WP_Bootstrap_Navwalker(),
+										//endbootstrapmenu
+									] );
 								?>
-
-							</div>
+							</nav>
 						</div>
 					</div>
 				</div>
