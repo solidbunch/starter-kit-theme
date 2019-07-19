96c2a763f7a415d33e87b7c370888537b89fe898
yurii-pavlov <mailyurik@gmail.com>
2019-07-19 20:40:56 +0300

    add security options

--- framework-customizations/theme/options/security.php
+++ framework-customizations/theme/options/security.php
@@ -34,6 +34,49 @@ $options = [
 					]
 				],
 
+				'xmlrpc_box' => [
+					'title'   => esc_html__( 'XML-RPC settings', 'starter-kit' ),
+					'type'    => 'box',
+					'attr'    => [
+						'class' => 'prevent-auto-close'
+					],
+					'options' => [
+
+						'enable_xmlrpc' => [
+							'type'         => 'switch',
+							'label'        => __( 'XML-RPC enable', 'starter-kit' ),
+							'right-choice' => [
+								'value' => '1',
+								'label' => __( 'Yes', 'starter-kit' )
+							],
+							'left-choice'  => [
+								'value' => '0',
+								'color' => '#ccc',
+								'label' => __( 'No', 'starter-kit' )
+							],
+							'desc'         => __( 'Enable XML-RPC API remote procedure call (RPC), which is disabled by default', 'starter-kit' ),
+
+						],
+
+						'disable_pingbacks' => [
+							'type'         => 'switch',
+							'label'        => __( 'Trackbacks/Pingbacks', 'starter-kit' ),
+							'right-choice' => [
+								'value' => '1',
+								'label' => __( 'Yes', 'starter-kit' )
+							],
+							'left-choice'  => [
+								'value' => '0',
+								'color' => '#ccc',
+								'label' => __( 'No', 'starter-kit' )
+							],
+							'desc'         => __( 'Disables trackbacks/pingbacks', 'starter-kit' ),
+
+						],
+
+					]
+				],
+
 			]
 		]
 	]
