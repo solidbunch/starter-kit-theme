e6e3c0476b7cb0f324db6a45245d601cc7d73de9
vvv3 <test.vvv@ukr.net>
2019-11-15 00:43:21 +0200

    config refactoring

---
+++ app/config/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Application config
+ *
+ * Return an array with predefined config values
+ *
+ * @category   Wordpress
+ * @package    Starter Kit Backend
+ * @author     SolidBunch
+ * @link       https://solidbunch.com
+ * @version    Release: 1.0.0
+ * @since      Class available since Release 1.0.0
+ */
+return [
+	'cache_time'          => '201812022311',
+	'assets_uri'          => get_template_directory_uri() . '/assets/',
+	'shortcodes_dir'      => get_template_directory() . '/app/Shortcodes/',
+	'shortcodes_uri'      => get_template_directory_uri() . '/app/Shortcodes/',
+	'shortcodes_icon_uri' => get_template_directory_uri() . '/assets/images/icon/',
+	'widgets_dir'         => get_template_directory() . '/app/Widgets/',
+	'widgets_uri'         => get_template_directory_uri() . '/app/Widgets/',
+	'social_profiles'     => include 'social_profiles.php',
+	'social_icons'        => include 'social_icons.php',
+	'animations'          => include 'animations.php',
+];
