632166ecca15ba23cf1c8d1faf09af070e83ae8d
Yurii Pavlov <mailyurik@gmail.com>
2019-12-05 20:06:12 +0200

    update antispam

--- assets/js/antispam.js
+++ assets/js/antispam.js
@@ -1,60 +1,64 @@
 (function ($) {
-	
+
 	"use strict";
-	
+
 	window.themeAntispam = {
-		
+
 		/**
 		 Constructor
 		 **/
 		initialize: function () {
-			
+
 			var self = this;
-			
+
 			$(document).ready(function () {
 				self.setupFormsAntispam();
 			});
-			
+
 		},
-		
-		
+
+
 		/**
 		 * Setup Antispam Field
 		 **/
 		setupFormsAntispam: function () {
-			
+
 			$('body').find('form').each(function () {
-				
 				if ($(this).attr('method').toUpperCase() === 'POST') {
-					
-					$(this).find('input[type=submit], button[type=submit]').each(function () {
-						try {
-							var sbmt = $(this)[0],
-								npt = document.createElement('input'),
-								d = new Date(),
-								__ksinit = function () {
-									sbmt.parentNode.insertBefore(npt, sbmt);
-								};
-							
-							npt.value = d.getUTCDate() + '' + (d.getUTCMonth() + 1) + Math.random();
-							npt.name = 'as_code';
-							npt.type = 'hidden';
-							sbmt.onmousedown = __ksinit;
-							sbmt.onkeypress = __ksinit;
-						} catch (e) {
-						}
-					});
+					try {
+						var $form = $(this),
+							d = new Date(),
+							code = d.getUTCHours() * d.getUTCDate() * (d.getUTCMonth() +1) * d.getUTCFullYear(),
+							name = 'a' + 's' + String.fromCharCode(95) + 'co' + 'de',
+							insert = function () {
+								if ($form.find('input[name="' + name + '"]').length === 0) {
+									$form.append('<input name="' + name + '" type="hidden" value="' + code + '">');
+								}
+							};
+
+						$form.mousedown(function() {
+							insert();
+						});
+
+						$form.keypress(function() {
+							insert();
+						});
+
+					} catch (e) {
+
+					}
+
 				}
-				
+
 			});
-			
+
 		},
-		
-		
+
+
 	}
-	
+
 	window.themeAntispam.initialize();
-	
+
 })(window.jQuery);
 
 
