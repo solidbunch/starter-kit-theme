38b8bd22ddd90ccf265e5561327f2db3d63d049d
Yurii Pavlov <mailyurik@gmail.com>
2023-10-23 21:38:34 +0200

    remove vc shortcoders support

--- app/Shortcodes/menu/view/view.php
+++
@@ -1,7 +0,0 @@
-
-	<nav id="<?php echo $data['atts']['el_id']; ?>" class="shortcode-menu flex-row-reverse navbar navbar-expand-lg navbar-light">
-		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#<?php echo $data['args']['container_id']; ?>" aria-controls="<?php echo $data['args']['container_id']; ?>" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<?php wp_nav_menu( $data['args'] ); ?>
-	</nav>
