e29aab7319e74544bd868df558796496421131aa
vvv3 <test.vvv@ukr.net>
2023-11-20 23:23:12 +0200

    Added windows filesystem compatibility; Removes assets files(css, js) from build folders once prior to Webpack compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,8 @@
 const mix = require('laravel-mix');
 const glob = require('glob');
 
+require('laravel-mix-clean');
+
 /**
  * Setup options
  * https://laravel-mix.com/docs/6.0/api#optionsoptions
@@ -41,31 +43,43 @@ if (!mix.inProduction()) {
   const StylelintPlugin = require('stylelint-webpack-plugin');
 
   mix
-    .sourceMaps()
-    .webpackConfig({
-      devtool: 'inline-source-map',
-      plugins: [
-        new ESLintPlugin({
-          fix: false,
-          extensions: ['js', 'jsx'],
-          overrideConfigFile: '.eslintrc.json',
-          failOnError: false,
-          cache: true
-        }),
-        new StylelintPlugin({
-          fix: false,
-          extensions: ['scss'],
-          configFile: '.stylelintrc.json',
-          failOnError: false,
-          files: [
-            'assets/src/**/*.scss',
-            'blocks/!(_)**/src/*.scss'
-          ],
-          cache: true,
-        }),
-      ]
-    });
+  .sourceMaps()
+  .webpackConfig({
+    devtool: 'inline-source-map',
+    plugins: [
+      new ESLintPlugin({
+        fix: false,
+        extensions: ['js', 'jsx'],
+        overrideConfigFile: '.eslintrc.json',
+        failOnError: false,
+        cache: true
+      }),
+      new StylelintPlugin({
+        fix: false,
+        extensions: ['scss'],
+        configFile: '.stylelintrc.json',
+        failOnError: false,
+        files: [
+          'assets/src/**/*.scss',
+          'blocks/!(_)**/src/*.scss'
+        ],
+        cache: true,
+      }),
+    ]
+  });
 }
+
+/**
+ * Remove assets files(css, js) from build folders
+ */
+mix.clean({
+  verbose: true,  // Write Logs to Console (Always enabled when dry is true)
+  dry: false, // Simulate the removal of files
+  cleanStaleWebpackAssets: false, // Automatically remove all unused webpack assets on rebuild
+  protectWebpackAssets: false, // Do not allow removal of current webpack assets
+  //Removes files once prior to Webpack compilation Not included in rebuilds (watch mode)
+  cleanOnceBeforeBuildPatterns: ['**/build/**/*.{css,js}', '!vendor/**', '!vendor-custom/**', '!node_modules/**']
+});
 
 /**
  * Read the folders and look for assets files.
@@ -77,16 +91,26 @@ if (!mix.inProduction()) {
  * Example, '_StarterBlock' - should not be registered
  */
 const allAssets = glob.sync('assets/src/**/!(_)*.@(scss|js|jsx)')
-  .concat(glob.sync('blocks/!(_)**/src/!(_)*.@(scss|js|jsx)'));
+.concat(glob.sync('blocks/!(_)**/src/!(_)*.@(scss|js|jsx)'));
 
 /**
  * Run Preprocessing
  */
 allAssets.forEach(assetPath => {
   if (assetPath.endsWith('.scss')) {
-    mix.sass(assetPath, assetPath.replace(/\/src\//, '/build/').replace(/\.(scss)$/, '.css'));
+    mix.sass(
+      assetPath,
+      assetPath.replace(/\/src\//, '/build/')
+      .replace(/\\src\\/, '\\build\\')
+      .replace(/\.(scss)$/, '.css')
+    );
   } else if (assetPath.endsWith('.js') || assetPath.endsWith('.jsx')) {
-    mix.js(assetPath, assetPath.replace(/\/src\//, '/build/').replace(/\.(jsx)$/, '.js'));
+    mix.js(
+      assetPath,
+      assetPath.replace(/\/src\//, '/build/')
+      .replace(/\\src\\/, '\\build\\')
+      .replace(/\.(jsx)$/, '.js')
+    );
   }
 });
 
