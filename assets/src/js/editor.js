a9f17653718bfa1991fbc36af0343dc13701ce90
David <halkin.david@gmail.com>
2023-12-21 18:15:00 +0200

    start editing external js

--- assets/src/js/editor.js
+++ assets/src/js/editor.js
@@ -1,13 +1,149 @@
+
+
 const {addFilter} = wp.hooks;
 const {createHigherOrderComponent} = wp.compose;
+const { InspectorControls, useBlockProps, InnerBlocks } = wp.blockEditor;
+const { PanelBody, SelectControl, RangeControl, CheckboxControl } = wp.components;
+// const { Fragment } = wp.element;
+
+const numberOfGrid = 5;
+
+const spacers = ['m', 'p'];
+const spacersName = ['t', 'b', 'e', 's'];
+
+// remove Restricted Classes
+function removeRestrictedClasses(inputStr, excludeArray) {
+  // Split the string into words
+  const words = inputStr.split(/\s+/);
+  // Remove matches from the list of words
+  const filteredWords = words.filter(word => !excludeArray.includes(word));
+  return filteredWords.join(' ');
+}
+
+// add classes
+function generateClasses(attributes, numberOfGrid) {
+  const { size, modification } = attributes;
+  const classes = [modification]; // Начинаем с класса modification (container)
+
+  Object.values(size).forEach((item) => {
+    if (item.valueRange) {
+      Object.keys(item.valueRange).forEach(i => {
+        const modifiedValue = item.valueRange[i] === (numberOfGrid + 1) ? 'auto' : item.valueRange[i];
+        const modifiedClass = `${i}-${modifiedValue}`.replace('-xs', '');
+        classes.push(modifiedClass);
+      });
+    }
+  });
+
+  return classes.join(' ');
+}
 
 const withCustomOption = createHigherOrderComponent((BlockEdit) => {
   return (props) => {
-    // Check if the block is from the specified category
-    if (props.name.includes('category-name')) {
-      // Add your custom option UI and functionality here
-    }
+    const { attributes, setAttributes, clientId, className }= props;
+    if (props.name.startsWith('starter-kit/')) {
+      return (
+        
+        <>
+          <BlockEdit {...props} />
+          <InspectorControls key="controls">
+          <PanelBody title="Spacers from EDITOR file">
+            {Object.keys(attributes.size).map((breakpoint) => (
 
+              <div key={breakpoint} title={`breakpoint settings - ${breakpoint}`} className={`box_breakpoint ${attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== '' ? 'active' : ''}`}>
+                <CheckboxControl
+                  label={`Enable ${breakpoint}`}
+                  checked={
+                    attributes.size && attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== ""
+                  }
+                  onChange={(isChecked) => {
+                    const sizeObject = { ...attributes.size };
+                    if (isChecked) {
+                      sizeObject[breakpoint] = { ...sizeObject[breakpoint], valueRange: {} };
+                    } else {
+                      // sizeObject[breakpoint] = { ...sizeObject[breakpoint], valueRange: "" };
+                      delete sizeObject[breakpoint].valueRange;
+                    }
+                    setAttributes({ ...attributes, size: sizeObject });
+                  }}
+                />
+
+                {attributes.size && attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== "" && (
+                  <>
+                    {spacers.map((spacer, index) => (
+                      spacersName.map((spacerName, innerIndex) => {
+                        const uniqueKey = `${spacer}${spacerName}-${breakpoint}`;
+                        const maxGrid = spacer === 'p' ? numberOfGrid : numberOfGrid + 1;
+
+                        return (
+                          <RangeControl
+                            key={uniqueKey}
+                            allowReset={false}
+                            label={
+                              (() => {
+                                let labelValue;
+                                if (attributes.size[breakpoint]?.valueRange?.[uniqueKey] !== undefined) {
+                                  if (attributes.size[breakpoint].valueRange[uniqueKey] === (numberOfGrid + 1)) {
+                                    labelValue = "auto";
+                                  } else {
+                                    labelValue = attributes.size[breakpoint].valueRange[uniqueKey];
+                                  }
+                                } else {
+                                  labelValue = "none";
+                                }
+                                return `${uniqueKey}-${labelValue}`;
+                              })()
+                            }
+
+
+                            value={
+                              (() => {
+                                if (attributes.size[breakpoint]?.valueRange?.[uniqueKey] !== undefined) {
+                                  return attributes.size[breakpoint].valueRange[uniqueKey];
+                                }
+                                return -1;
+                              })()
+                            }
+
+                            onChange={value => {
+                              const sizeObject = { ...attributes.size };
+
+                              if (value === -1) {
+
+                                delete sizeObject[breakpoint].valueRange[uniqueKey];
+                              } else {
+
+                                sizeObject[breakpoint] = {
+                                  ...sizeObject[breakpoint],
+                                  valueRange: {
+                                    ...sizeObject[breakpoint].valueRange,
+
+                                    [uniqueKey]: value,
+                                  },
+                                };
+                              }
+                              setAttributes({ ...attributes, size: sizeObject });
+                            }}
+
+                            min={-1}
+                            max={maxGrid}
+                            withInputField={false}
+                            {...props}
+                          />
+                        );
+                      })
+                    ))}
+                  </>
+                )}
+
+
+              </div>
+            ))}
+          </PanelBody>
+        </InspectorControls>
+        </>
+      );
+    }
     return <BlockEdit {...props} />;
   };
 }, 'withCustomOption');
--- blocks/Container/src/index.jsx
+++ blocks/Container/src/index.jsx
@@ -12,8 +12,8 @@ const { InspectorControls, useBlockProps, InnerBlocks } = wp.blockEditor;
 const { PanelBody, SelectControl, RangeControl, CheckboxControl } = wp.components;
 const numberOfGrid = 5;
 
-const spacers = ['m', 'p'];
-const spacersName = ['t', 'b', 'e', 's'];
+// const spacers = ['m', 'p'];
+// const spacersName = ['t', 'b', 'e', 's'];
 // remove Restricted Classes
 function removeRestrictedClasses(inputStr, excludeArray) {
   // Split the string into words
@@ -69,11 +69,11 @@ registerBlockType(
               value={attributes.modification}
               options={[
                 // ToDo - add breakpoints options
-                //{label: 'container', value: 'container'},
-                //{label: 'container-sm', value: 'container-sm'},
-                //{label: 'container-md', value: 'container-md'},
-                //{label: 'container-lg', value: 'container-lg'},
-                //{label: 'container-xl', value: 'container-xl'},
+                { label: 'container', value: 'container' },
+                { label: 'container-sm', value: 'container-sm' },
+                { label: 'container-md', value: 'container-md' },
+                { label: 'container-lg', value: 'container-lg' },
+                { label: 'container-xl', value: 'container-xl' },
                 { label: 'Fixed width', value: 'container-xxl' },
                 { label: 'Full width', value: 'container-fluid' }
               ]}
@@ -81,101 +81,101 @@ registerBlockType(
             />
           </PanelBody>
         </InspectorControls>,
-        <InspectorControls key="controls">
-          <PanelBody title="Spacers">
-            {Object.keys(attributes.size).map((breakpoint) => (
+        // <InspectorControls key="controls">
+        //   <PanelBody title="Spacers">
+        //     {Object.keys(attributes.size).map((breakpoint) => (
 
-              <div key={breakpoint} title={`breakpoint settings - ${breakpoint}`} className={`box_breakpoint ${attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== '' ? 'active' : ''}`}>
-                <CheckboxControl
-                  label={`Enable ${breakpoint}`}
-                  checked={
-                    attributes.size && attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== ""
-                  }
-                  onChange={(isChecked) => {
-                    const sizeObject = { ...attributes.size };
-                    if (isChecked) {
-                      sizeObject[breakpoint] = { ...sizeObject[breakpoint], valueRange: {} };
-                    } else {
-                      // sizeObject[breakpoint] = { ...sizeObject[breakpoint], valueRange: "" };
-                      delete sizeObject[breakpoint].valueRange;
-                    }
-                    setAttributes({ ...attributes, size: sizeObject });
-                  }}
-                />
+        //       <div key={breakpoint} title={`breakpoint settings - ${breakpoint}`} className={`box_breakpoint ${attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== '' ? 'active' : ''}`}>
+        //         <CheckboxControl
+        //           label={`Enable ${breakpoint}`}
+        //           checked={
+        //             attributes.size && attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== ""
+        //           }
+        //           onChange={(isChecked) => {
+        //             const sizeObject = { ...attributes.size };
+        //             if (isChecked) {
+        //               sizeObject[breakpoint] = { ...sizeObject[breakpoint], valueRange: {} };
+        //             } else {
+        //               // sizeObject[breakpoint] = { ...sizeObject[breakpoint], valueRange: "" };
+        //               delete sizeObject[breakpoint].valueRange;
+        //             }
+        //             setAttributes({ ...attributes, size: sizeObject });
+        //           }}
+        //         />
 
-                {attributes.size && attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== "" && (
-                  <>
-                    {spacers.map((spacer, index) => (
-                      spacersName.map((spacerName, innerIndex) => {
-                        const uniqueKey = `${spacer}${spacerName}-${breakpoint}`;
-                        const maxGrid = spacer === 'p' ? numberOfGrid : numberOfGrid + 1;
+        //         {attributes.size && attributes.size[breakpoint].valueRange !== undefined && attributes.size[breakpoint].valueRange !== "" && (
+        //           <>
+        //             {spacers.map((spacer, index) => (
+        //               spacersName.map((spacerName, innerIndex) => {
+        //                 const uniqueKey = `${spacer}${spacerName}-${breakpoint}`;
+        //                 const maxGrid = spacer === 'p' ? numberOfGrid : numberOfGrid + 1;
 
-                        return (
-                          <RangeControl
-                            key={uniqueKey}
-                            allowReset={false}
-                            label={
-                              (() => {
-                                let labelValue;
-                                if (attributes.size[breakpoint]?.valueRange?.[uniqueKey] !== undefined) {
-                                  if (attributes.size[breakpoint].valueRange[uniqueKey] === (numberOfGrid + 1)) {
-                                    labelValue = "auto";
-                                  } else {
-                                    labelValue = attributes.size[breakpoint].valueRange[uniqueKey];
-                                  }
-                                } else {
-                                  labelValue = "none";
-                                }
-                                return `${uniqueKey}-${labelValue}`;
-                              })()
-                            }
-
-
-                            value={
-                              (() => {
-                                if (attributes.size[breakpoint]?.valueRange?.[uniqueKey] !== undefined) {
-                                  return attributes.size[breakpoint].valueRange[uniqueKey];
-                                }
-                                return -1;
-                              })()
-                            }
-
-                            onChange={value => {
-                              const sizeObject = { ...attributes.size };
-
-                              if (value === -1) {
-
-                                delete sizeObject[breakpoint].valueRange[uniqueKey];
-                              } else {
-
-                                sizeObject[breakpoint] = {
-                                  ...sizeObject[breakpoint],
-                                  valueRange: {
-                                    ...sizeObject[breakpoint].valueRange,
-
-                                    [uniqueKey]: value,
-                                  },
-                                };
-                              }
-                              setAttributes({ ...attributes, size: sizeObject });
-                            }}
-
-                            min={-1}
-                            max={maxGrid}
-                            withInputField={false}
-                            {...props}
-                          />
-                        );
-                      })
-                    ))}
-                  </>
-                )}
+        //                 return (
+        //                   <RangeControl
+        //                     key={uniqueKey}
+        //                     allowReset={false}
+        //                     label={
+        //                       (() => {
+        //                         let labelValue;
+        //                         if (attributes.size[breakpoint]?.valueRange?.[uniqueKey] !== undefined) {
+        //                           if (attributes.size[breakpoint].valueRange[uniqueKey] === (numberOfGrid + 1)) {
+        //                             labelValue = "auto";
+        //                           } else {
+        //                             labelValue = attributes.size[breakpoint].valueRange[uniqueKey];
+        //                           }
+        //                         } else {
+        //                           labelValue = "none";
+        //                         }
+        //                         return `${uniqueKey}-${labelValue}`;
+        //                       })()
+        //                     }
 
 
-              </div>
-            ))}
-          </PanelBody>
-        </InspectorControls>,
+        //                     value={
+        //                       (() => {
+        //                         if (attributes.size[breakpoint]?.valueRange?.[uniqueKey] !== undefined) {
+        //                           return attributes.size[breakpoint].valueRange[uniqueKey];
+        //                         }
+        //                         return -1;
+        //                       })()
+        //                     }
+
+        //                     onChange={value => {
+        //                       const sizeObject = { ...attributes.size };
+
+        //                       if (value === -1) {
+
+        //                         delete sizeObject[breakpoint].valueRange[uniqueKey];
+        //                       } else {
+
+        //                         sizeObject[breakpoint] = {
+        //                           ...sizeObject[breakpoint],
+        //                           valueRange: {
+        //                             ...sizeObject[breakpoint].valueRange,
+
+        //                             [uniqueKey]: value,
+        //                           },
+        //                         };
+        //                       }
+        //                       setAttributes({ ...attributes, size: sizeObject });
+        //                     }}
+
+        //                     min={-1}
+        //                     max={maxGrid}
+        //                     withInputField={false}
+        //                     {...props}
+        //                   />
+        //                 );
+        //               })
+        //             ))}
+        //           </>
+        //         )}
+
+
+        //       </div>
+        //     ))}
+        //   </PanelBody>
+        // </InspectorControls>,
         <div {...blockProps} key="blockControls">
           <InnerBlocks
             renderAppender={
