1bb693fa1321cb85fad8d6fff24c53df769391ff
David <halkin.david@gmail.com>
2024-01-04 15:26:36 +0200

    start navigation

---
+++ blocks/Navigation/src/index.jsx
@@ -0,0 +1,76 @@
+/**
+ * Block dependencies
+ */
+import metadata from '../block.json';
+
+/**
+ * Internal block libraries
+ */
+const {registerBlockType} = wp.blocks;
+const {useSelect} = wp.data;
+const {InspectorControls, useBlockProps, InnerBlocks} = wp.blockEditor;
+const {PanelBody, SelectControl} = wp.components;
+
+registerBlockType(
+  metadata,
+  {
+    getEditWrapperProps(attributes) {
+      const blockClass = attributes.modification;
+      return {className: blockClass};
+    },
+    edit: props => {
+      const {attributes, setAttributes, clientId, className} = props;
+      const blockProps = useBlockProps({
+        className: [className],
+      });
+      const {hasChildBlocks} = useSelect((select) => {
+        const {getBlockOrder} = select('core/block-editor');
+
+        return {
+          hasChildBlocks: getBlockOrder(clientId).length > 0,
+        };
+      });
+
+      return [
+        <InspectorControls key="controls">
+          <PanelBody title="Container responsive type">
+            <h1>NAV</h1>
+            <SelectControl
+              label="Container width"
+              value={attributes.modification}
+              options={[
+                {label: 'Fixed width', value: 'container-xxl'},
+                {label: 'Full width', value: 'container-fluid'}
+              ]}
+              onChange={(modification) => setAttributes({modification})}
+            />
+          </PanelBody>
+        </InspectorControls>,
+        <div {...blockProps} key="blockControls">
+          <InnerBlocks
+            renderAppender={
+              hasChildBlocks
+                ? undefined
+                : () => <InnerBlocks.ButtonBlockAppender />
+            }
+          />
+        </div>
+      ];
+    },
+    save: props => {
+      const {attributes} = props;
+
+      const blockClass = attributes.modification;
+
+      const blockProps = useBlockProps.save({
+        className: blockClass
+      });
+
+      return (
+        <div {...blockProps}>
+          <InnerBlocks.Content />
+        </div>
+      );
+    }
+  }
+);
