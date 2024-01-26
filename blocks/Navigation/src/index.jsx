3485cf353ef2a9f58f43e498f24c938ae89838ca
Yurii Pavlov <mailyurik@gmail.com>
2024-01-26 13:41:55 +0100

    update header menu location select in navigation blocks

--- blocks/Navigation/src/index.jsx
+++ blocks/Navigation/src/index.jsx
@@ -24,9 +24,6 @@ registerBlockType(
       const [menuLocations, setMenuLocations] = useState([]);
       const [menus, setMenus] = useState([]);
 
-      // eslint-disable-next-line no-console
-      console.log(menuLocations);
-
       useEffect(() => {
         wp.apiFetch({path: '/skt/v1/get-menu-locations'})
           .then(fetchedMenuLocations => {
@@ -50,12 +47,33 @@ registerBlockType(
       const renderControls = (
         <InspectorControls key="controls">
           <PanelBody title="Navigation Options">
+            {menuLocations.length < 1
+              ? <Spinner key="siteSpinner"/>
+              : (
+                <>
+                  <SelectControl
+                    label="Select Menu Location"
+                    value={attributes.menuLocation}
+                    options={[
+                      {label: 'Select a location', value: ''},
+                      ...menuLocations.map(location => ({
+                        label: location.name,
+                        value: location.slug,
+                      })),
+                    ]}
+                    onChange={(menuLocation) => {
+                      setAttributes({menuLocation});
+                    }}
+                  />
+                </>
+              )
+            }
             {menus.length < 1
               ? <Spinner key="siteSpinner"/>
               : (
                 <>
                   <SelectControl
-                    label="Select Menu"
+                    label="Or Select specific Menu (optional)"
                     value={attributes.menuId}
                     options={[
                       {label: 'Select a menu', value: ''},
@@ -68,26 +86,23 @@ registerBlockType(
                       setAttributes({menuId});
                     }}
                   />
-                  {attributes.menuId && (
-                    <SelectControl
-                      label={`Expand menu:`}
-                      value={attributes.expand}
-                      options={[
-                        {label: 'No responsive', value: 'navbar-expand'},
-                        {label: 'SM and under', value: 'navbar-expand-sm'},
-                        {label: 'MD and under', value: 'navbar-expand-md'},
-                        {label: 'LG and under', value: 'navbar-expand-lg'},
-                        {label: 'XL and under', value: 'navbar-expand-xl'},
-                        {label: 'XXL and under', value: 'navbar-expand-xxl'},
-                        {label: 'Always expand', value: ''},
-                      ]}
-                      onChange={(expand) => setAttributes({expand})}
-                    />
-                  )}
                 </>
               )
             }
-
+            <SelectControl
+              label={`Expand menu:`}
+              value={attributes.expand}
+              options={[
+                {label: 'No responsive', value: 'navbar-expand'},
+                {label: 'SM and under', value: 'navbar-expand-sm'},
+                {label: 'MD and under', value: 'navbar-expand-md'},
+                {label: 'LG and under', value: 'navbar-expand-lg'},
+                {label: 'XL and under', value: 'navbar-expand-xl'},
+                {label: 'XXL and under', value: 'navbar-expand-xxl'},
+                {label: 'Always expand', value: ''},
+              ]}
+              onChange={(expand) => setAttributes({expand})}
+            />
           </PanelBody>
         </InspectorControls>
       );
