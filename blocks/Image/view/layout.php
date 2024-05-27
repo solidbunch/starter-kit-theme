3399d05c8bbb7751350a11d0c2d80710e2e535c2
Yurii Pavlov <mailyurik@gmail.com>
2024-05-27 16:01:35 +0200

    update link href template check

--- blocks/Image/view/layout.php
+++ blocks/Image/view/layout.php
@@ -13,7 +13,7 @@ $data = $data ?? [];
 ?>
 
 <figure<?php echo !empty($data['blockClass']) ? ' class="' . $data['blockClass'] . '"' : ''; ?>>
-    <?php if (!empty($data['link']['addLink']) && !empty($data['link']['href'])) { ?>
+    <?php if (!empty($data['link']['addLink'])) { ?>
         <a href="<?php echo $data['link']['href']; ?>"<?php echo !empty($data['link']['targetBlank']) ? ' target="_blank"' : ''; ?>>
             <?php echo $data['imgHtml'] ?? ''; ?>
         </a>
