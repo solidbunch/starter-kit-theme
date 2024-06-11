c777d36e526b5cce0e6435453cf00b99753ba3d5
David <halkin.david@gmail.com>
2024-06-11 17:01:06 +0300

    clean

--- blocks/PricingTable/src/index.jsx
+++ blocks/PricingTable/src/index.jsx
@@ -7,16 +7,14 @@ import metadata from '../block.json';
  * Internal block libraries
  */
 const {registerBlockType} = wp.blocks;
-const {InspectorControls, useBlockProps} = wp.blockEditor;
-const {PanelBody, SelectControl, Spinner} = wp.components;
+const {useBlockProps} = wp.blockEditor;
 const {serverSideRender: ServerSideRender} = wp;
-const {useState, useEffect} = wp.element;
 
 registerBlockType(
   metadata,
   {
     edit: props => {
-      const {attributes, setAttributes, className} = props;
+      const {attributes, className} = props;
       const blockProps = useBlockProps({
         className: [className],
       });
