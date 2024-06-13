893b73546cce3857647583a2dbb5173e8c5343a8
David <halkin.david@gmail.com>
2024-06-13 16:39:37 +0300

    layout add

--- blocks/DocPagesNav/view/layout.php
+++ blocks/DocPagesNav/view/layout.php
@@ -11,41 +11,28 @@ defined('ABSPATH') || exit;
 $data = $data ?? [];
 
 ?>
-
-
-
-<section class="section_doc d-flex flex-wrap align-items-start py-5">
-    <aside class="sidebar col-12 py-3">
-        <button class="btn btn-primary navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDocs" aria-controls="offcanvasDocs" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas-lg offcanvas-start " tabindex="-1" id="offcanvasDocs" aria-labelledby="offcanvasDocsLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasDocsLabel">Docs:</h5>
-                <button type="button" class="btn-close ms-auto" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#offcanvasDocs"></button>
-            </div>
-            <div class="offcanvas-body">
-                <div class="offcanvas_content">
-                    <?php for ($i = 0; $i < 5; $i++): ?>
-                        <div class="py-3 px-lg-3">
-                            <h5><?= $i ?>  title</h5>
-                            <ul class="list-unstyled list_doc">
-                                <li><a href="#"> Lorem, ipsum dolor sit amet dolor sit amet</a></li>
-                                <li><a href="#"  class="active">  sit amet</a></li>
-                                <li><a href="#"> Lorem, ipsum</a></li>
-                            </ul>
-                        </div>
-                    <?php endfor; ?>
-                </div>
+<aside class="sidebar col-12 py-1 px-3 py-lg-3 px-lg-0">
+    <button class="btn btn-outline-primary p-1 doc_toggler_btn navbar-toggler d-lg-none " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDocs" aria-controls="offcanvasDocs" aria-label="Toggle navigation">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" aria-hidden="true" focusable="false" viewBox="0 0 24 24"><path d="M17,11H3c-0.6,0-1-0.4-1-1s0.4-1,1-1h14c0.6,0,1,0.4,1,1S17.6,11,17,11z"></path><path d="M21,7H3C2.4,7,2,6.6,2,6s0.4-1,1-1h18c0.6,0,1,0.4,1,1S21.6,7,21,7z"></path><path d="M21,15H3c-0.6,0-1-0.4-1-1s0.4-1,1-1h18c0.6,0,1,0.4,1,1S21.6,15,21,15z"></path><path d="M17,19H3c-0.6,0-1-0.4-1-1s0.4-1,1-1h14c0.6,0,1,0.4,1,1S17.6,19,17,19z"></path></svg>
+    </button>
+    <div class="offcanvas-lg offcanvas-start doc_offcanvas" tabindex="-1" id="offcanvasDocs" aria-labelledby="offcanvasDocsLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDocsLabel">Docs:</h5>
+            <button type="button" class="btn-close ms-auto" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#offcanvasDocs"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div class="offcanvas_content">
+                <?php for ($i = 0; $i < 2; $i++): ?>
+                    <div class="py-3 px-lg-3">
+                        <h5>Title <?= $i ?>  </h5>
+                        <ul class="list-unstyled list_doc">
+                            <li><a href="#"> Lorem, ipsum dolor sit amet dolor sit amet</a></li>
+                            <li><a href="#"  class="active">  sit amet</a></li>
+                            <li><a href="#"> Lorem, ipsum</a></li>
+                        </ul>
+                    </div>
+                <?php endfor; ?>
             </div>
         </div>
-    </aside>
-    <article class="col">
-        <div class="container">
-        <?php for ($i = 0; $i < 100; $i++): ?>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis libero illo fuga labore architecto eveniet, nam officia similique error perferendis enim, vitae, iure saepe. Quaerat odio ipsum saepe. Magni, rerum!</p>
-        <?php endfor; ?>
-        </div>
-    </article>
-</section>
-
+    </div>
+</aside>
