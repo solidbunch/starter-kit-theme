d2f0c7b1d01cef3eda5ef5f488970f7338a3fc8c
David <halkin.david@gmail.com>
2024-07-04 11:58:43 +0300

    clean

--- blocks/Code/src/view.js
+++ blocks/Code/src/view.js
@@ -37,7 +37,7 @@ import hljs from 'highlight.js';
     
     // change icon 2 seconds
     const originalIcon = button.innerHTML;
-    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="ok" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
+    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
     button.classList.add('active');
 
     setTimeout(() => {
