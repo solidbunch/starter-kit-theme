8b6435c0717a4600d6cc9440fd09ee0f7f9a992d
David <halkin.david@gmail.com>
2024-07-19 18:55:36 +0300

    add variables for row Block

--- blocks/Row/src/index.jsx
+++ blocks/Row/src/index.jsx
@@ -2,7 +2,7 @@
  * Block dependencies
  */
 import metadata from '../block.json';
-
+import variables from '../../../assets/build/variables.json';
 /**
  * Internal block libraries
  */
@@ -13,12 +13,23 @@ const {PanelBody, SelectControl, CheckboxControl} = wp.components;
 const blocksAllowed = ['starter-kit/column'];
 const blockTemplate = [['starter-kit/column'], ['starter-kit/column']];
 
+const gridBreakpoints = variables['grid-breakpoints'];
+
+// Initialize aligmentProperties with empty objects for each breakpoint
+const initializeAligmentProperties = () => {
+  const aligmentProperties = {};
+  Object.keys(gridBreakpoints).forEach(breakpoint => {
+    aligmentProperties[breakpoint] = {};
+  });
+  return aligmentProperties;
+};
+
 function getClasses(attributes) {
-  const {modification, properties} = attributes;
+  const {modification, aligmentProperties} = attributes;
   const classes = [modification]; // Начинаем с класса modification (row)
 
-  Object.keys(properties).forEach((breakpoint) => {
-    const {justifyContent, alignItems} = properties[breakpoint];
+  Object.keys(aligmentProperties).forEach((breakpoint) => {
+    const {justifyContent, alignItems} = aligmentProperties[breakpoint];
     const breakpointSuffix = breakpoint === "xs" ? "" : `-${breakpoint}`;
 
     if (justifyContent) {
@@ -40,47 +51,50 @@ registerBlockType(
       return {className: getClasses(attributes)};
     },
     edit: props => {
-
+      
       const {attributes, setAttributes, className} = props;
+      // Initialize aligmentProperties if not already initialized
+      if (!attributes.aligmentProperties || Object.keys(attributes.aligmentProperties).length === 0) {
+        setAttributes({aligmentProperties: initializeAligmentProperties()});
+      }
       const blockProps = useBlockProps({
         className: [className],
       });
-
       const renderControls = (
         <InspectorControls key="settings">
           <PanelBody title="alignment x" initialOpen={false}>
-            {Object.keys(attributes.properties).map((breakpoint) => (
+            {Object.keys(attributes.aligmentProperties).map((breakpoint) => (
 
               <div key={breakpoint} title={`Column settings - ${breakpoint}`} className={`box_breakpoint `}>
                 <CheckboxControl
                   label={`${breakpoint} alignment`}
                   checked={
-                    attributes.properties && attributes.properties[breakpoint].justifyContent !== undefined && attributes.properties[breakpoint].justifyContent !== ""
+                    attributes.aligmentProperties && attributes.aligmentProperties[breakpoint].justifyContent !== undefined && attributes.aligmentProperties[breakpoint].justifyContent !== ""
                   }
                   onChange={(isChecked) => {
-                    const propObject = {...attributes.properties};
+                    const propObject = {...attributes.aligmentProperties};
                     if (isChecked) {
                       propObject[breakpoint] = {...propObject[breakpoint], justifyContent: "start"};
                     } else {
                       propObject[breakpoint] = {...propObject[breakpoint], justifyContent: ""};
                     }
-                    setAttributes({...attributes, properties: propObject});
+                    setAttributes({...attributes, aligmentProperties: propObject});
 
                   }}
                 />
 
-                {attributes.properties && attributes.properties[breakpoint].justifyContent !== undefined && attributes.properties[breakpoint].justifyContent !== "" && (
+                {attributes.aligmentProperties && attributes.aligmentProperties[breakpoint].justifyContent !== undefined && attributes.aligmentProperties[breakpoint].justifyContent !== "" && (
                   <SelectControl
                     label={`Size ${breakpoint}`}
-                    value={attributes.properties[breakpoint].justifyContent}
+                    value={attributes.aligmentProperties[breakpoint].justifyContent}
                     options={attributes.justifyContent.map((value) => ({
                       label: value,
                       value,
                     }))}
                     onChange={(value) => {
-                      const propObject = {...attributes.properties};
+                      const propObject = {...attributes.aligmentProperties};
                       propObject[breakpoint] = {...propObject[breakpoint], justifyContent: value};
-                      setAttributes({...attributes, properties: propObject});
+                      setAttributes({...attributes, aligmentProperties: propObject});
                     }}
                   />
 
@@ -90,38 +104,38 @@ registerBlockType(
 
           </PanelBody>
           <PanelBody title="alignment Y" initialOpen={false}>
-            {Object.keys(attributes.properties).map((breakpoint) => (
+            {Object.keys(attributes.aligmentProperties).map((breakpoint) => (
 
               <div key={breakpoint} title={`Column settings - ${breakpoint}`} className={`box_breakpoint `}>
                 <CheckboxControl
                   label={`${breakpoint} alignment`}
                   checked={
-                    attributes.properties && attributes.properties[breakpoint].alignItems !== undefined && attributes.properties[breakpoint].alignItems !== ""
+                    attributes.aligmentProperties && attributes.aligmentProperties[breakpoint].alignItems !== undefined && attributes.aligmentProperties[breakpoint].alignItems !== ""
                   }
                   onChange={(isChecked) => {
-                    const propObject = {...attributes.properties};
+                    const propObject = {...attributes.aligmentProperties};
                     if (isChecked) {
                       propObject[breakpoint] = {...propObject[breakpoint], alignItems: "start"};
                     } else {
                       propObject[breakpoint] = {...propObject[breakpoint], alignItems: ""};
                     }
-                    setAttributes({...attributes, properties: propObject});
+                    setAttributes({...attributes, aligmentProperties: propObject});
 
                   }}
                 />
 
-                {attributes.properties && attributes.properties[breakpoint].alignItems !== undefined && attributes.properties[breakpoint].alignItems !== "" && (
+                {attributes.aligmentProperties && attributes.aligmentProperties[breakpoint].alignItems !== undefined && attributes.aligmentProperties[breakpoint].alignItems !== "" && (
                   <SelectControl
                     label={`Size ${breakpoint}`}
-                    value={attributes.properties[breakpoint].alignItems}
+                    value={attributes.aligmentProperties[breakpoint].alignItems}
                     options={attributes.alignItems.map((value) => ({
                       label: value,
                       value,
                     }))}
                     onChange={(value) => {
-                      const propObject = {...attributes.properties};
+                      const propObject = {...attributes.aligmentProperties};
                       propObject[breakpoint] = {...propObject[breakpoint], alignItems: value};
-                      setAttributes({...attributes, properties: propObject});
+                      setAttributes({...attributes, aligmentProperties: propObject});
                     }}
                   />
 
