0202aec9aec2995f7d1d502576a5aa364d236ac0
Yurii Pavlov <mailyurik@gmail.com>
2025-06-18 15:13:42 +0100

    update highlight.js connection, use default lang plaintext and theme atom-one-light

--- blocks/Code/src/view.js
+++ blocks/Code/src/view.js
@@ -1,16 +1,17 @@
 const hljs = require('highlight.js/lib/core');
 
 hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
+hljs.registerLanguage('ini', require('highlight.js/lib/languages/ini'));
 hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
 hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
+hljs.registerLanguage('plaintext', require('highlight.js/lib/languages/plaintext'));
 hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
-hljs.registerLanguage('vim', require('highlight.js/lib/languages/vim'));
-hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+//hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
 
-(function () {
-  "use strict";
+(function() {
+  'use strict';
 
   window.addEventListener('load', () => {
     highlightElement();
@@ -19,14 +20,31 @@ hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
 
   function highlightElement() {
     document.querySelectorAll('pre code').forEach((block) => {
-      hljs.highlightElement(block);
+      const className = block.className || '';
+
+      // If the language is specified, we use the standard backlight
+      if (className.includes('language-')) {
+        hljs.highlightElement(block);
+      } else {
+        // auto + fallback
+        const result = hljs.highlightAuto(block.textContent);
+
+        if (result.relevance < 5) {
+          const fallback = hljs.highlight(block.textContent, {language: 'plaintext'});
+          block.innerHTML = fallback.value;
+          block.classList.add('hljs', 'language-plaintext');
+        } else {
+          block.innerHTML = result.value;
+          block.classList.add('hljs', `language-${result.language}`);
+        }
+      }
     });
   }
 
   function addCopyButtons() {
     document.querySelectorAll('pre').forEach((pre) => {
       const button = document.createElement('button');
-      button.classList.add('btn','copy_clipboard');
+      button.classList.add('btn', 'copy_clipboard');
       button.innerHTML = '<i class="sk-icon sk-copy"></i>';
       button.dataset.originalIcon = button.innerHTML;
       button.dataset.timeoutId = '';
