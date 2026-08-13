f101c98a5a73e33f9aa04053823acb477cbc0bfa
Yurii Pavlov <mailyurik@gmail.com>
2026-08-13 18:47:28 +0100

    CleanUp update

--- src/Handlers/Optimization/CleanUp.php
+++ src/Handlers/Optimization/CleanUp.php
@@ -74,13 +74,15 @@ class CleanUp
          *            Remove emoji
          ************************************/
         remove_action('wp_head', 'print_emoji_detection_script', 7);
-        remove_action('admin_print_scripts', 'print_emoji_detection_script');
         remove_action('wp_print_styles', 'print_emoji_styles');
-        remove_action('admin_print_styles', 'print_emoji_styles');
         remove_filter('the_content_feed', 'wp_staticize_emoji');
         remove_filter('comment_text_rss', 'wp_staticize_emoji');
         remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
         add_filter('emoji_svg_url', '__return_false');
+        add_action('admin_init', function () {
+            remove_action('admin_print_scripts', 'print_emoji_detection_script');
+            remove_action('admin_print_styles', 'print_emoji_styles');
+        });
 
         /************************************
          *      Remove embed and oembed
